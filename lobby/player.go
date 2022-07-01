84b172bc8caf9b34d1101d62c506184831083f49
Axel Lijdens <axel.lijdens@wildlifestudios.com>
2022-07-01 00:38:06 -0300

    Update the lobby screen to show progress messages. Also fixed some goroutine leaks.

--- lobby/player.go
+++ lobby/player.go
@@ -2,21 +2,33 @@ package lobby
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"time"
 
 	"github.com/google/uuid"
 )
 
-func (player *Player) StartPlayer(ctx context.Context, mm *MatchMaker, matchResponse chan *Match) {
+func (player *Player) StartPlayer(ctx context.Context, mm *MatchMaker, matchResponse chan interface{}) {
 	log.Printf("Start player %+v\n", player)
+
+	matchResponse <- fmt.Sprintf("Start matchmaking with ELO: %v", player.elo)
+
+	defer close(matchResponse)
 
 	for {
 		select {
+		case <-ctx.Done():
+			return
 		case player2 := <-player.playersQueue:
 			log.Printf("Player %v entering Case...\n", player.Id)
-			if player2.Id == player.Id || !player.isValidMatch(player2) || !player2.isValidMatch(player) {
+
+			if player2.Id == player.Id {
+				continue
+			}
+			if !player.isValidMatch(player2) || !player2.isValidMatch(player) {
 				log.Printf("Player %v leaving Case...\n", player.Id)
+				matchResponse <- fmt.Sprintf("Tried to match with %v (ELO %v) but was not accepted", player2.Id, player2.elo)
 				continue
 			}
 
@@ -44,7 +56,7 @@ func (player *Player) StartPlayer(ctx context.Context, mm *MatchMaker, matchResp
 				log.Printf("Set player %v and %v in match\n", player.Id, player2.Id)
 				select {
 				case player2.matchQueue <- &match:
-				case <-time.After(time.Second * 15):
+				case <-time.After(time.Second * 5):
 					log.Panicf("Failed sending game Id to player '%v'", player2.Id)
 				}
 
@@ -53,28 +65,29 @@ func (player *Player) StartPlayer(ctx context.Context, mm *MatchMaker, matchResp
 				log.Printf("Unlocked both players\n")
 				select {
 				case matchResponse <- &match:
-				case <-time.After(time.Second * 15):
+				case <-time.After(time.Second * 5):
 					log.Panicf("Failed sending match to matchResponse ")
 				}
+				log.Printf("Player %v created game correctly\n", player.Id)
+				return
 			} else {
 				mm.Add(ctx, player2.Id)
 				player2.mtx.Unlock()
 				player.mtx.Unlock()
 			}
-
-			log.Printf("Player %v created game correctly\n", player.Id)
-
 		case matchedGame := <-player.matchQueue:
 			select {
 			case matchResponse <- matchedGame:
-			case <-time.After(time.Second * 15):
+			case <-time.After(time.Second * 5):
 				log.Printf("Failed sending match to response\n")
 			}
+			return
 
-		case <-time.After(time.Second * 30):
+		case <-time.After(time.Second * 5):
 			log.Printf("Player %v Relaxing Requirements...\n", player.Id)
 			player.relaxRequirements *= 1.03
 			mm.Add(ctx, player.Id)
+			matchResponse <- fmt.Sprintf("Relaxing matchmaking requirements: %v", player.relaxRequirements)
 		}
 
 	}
